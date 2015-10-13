6e74371b6548a6530d3a24b619e4a5eaeb69db4e
Michael Loistl <michael@aplo.co>
2015-10-13 16:27:30 +0100

    Initial Commit

---
+++ FormKit/FormKit.h
@@ -0,0 +1,19 @@
+//
+//  FormKit.h
+//  FormKit
+//
+//  Created by Michael Loistl on 13/10/2015.
+//  Copyright © 2015 Aplo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FormKit.
+FOUNDATION_EXPORT double FormKitVersionNumber;
+
+//! Project version string for FormKit.
+FOUNDATION_EXPORT const unsigned char FormKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormKit/PublicHeader.h>
+
+
